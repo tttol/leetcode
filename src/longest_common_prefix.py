9d9c76361c40112e62d921f8468824cc8d1c309a
tttol <tkhstol.929@gmail.com>
2025-05-15 17:46:44 +0900

    add

--- src/longest_common_prefix.py
+++ src/longest_common_prefix.py
@@ -5,19 +5,22 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        prefix = strs.pop()
-        for s in strs:
-            if len(s) < len(prefix):
-                continue
-            
-            i = 0
-            while i < min(len(s), len(prefix)):
-                if s[i] != prefix[i]:
-                    prefix = s[:i]
-                    break
-                i += 1
-        return prefix
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+
+        ans = ""
+        for i in range(len(first)):
+            if first[i] != last[i]:
+                break
+            ans += first[i]
+        return ans
 
 s = Solution()
+print(s.longestCommonPrefix(["reflower","flow","flight"]))
+print(s.longestCommonPrefix(["a"]))
+print(s.longestCommonPrefix([""]))
 print(s.longestCommonPrefix(["flower","flow","flight"]))
 print(s.longestCommonPrefix(["dog","racecar","car"]))
+print(s.longestCommonPrefix(["minimum","min"]))
+print(s.longestCommonPrefix(["","b"]))
