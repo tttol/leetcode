3044d8921560cefb2845b0912af067f512ca9491
tttol <tkhstol.929@gmail.com>
2025-05-19 15:11:51 +0900

    fix

--- src/product_of_array_except_self.py
+++ src/product_of_array_except_self.py
@@ -1,24 +1,38 @@
 class Solution(object):
+    def dp(self, target_arr, initial_value):
+        n = len(target_arr)
+        dp = [1] * n
+        dp[0] = initial_value
+        for i in range(1, n):
+            dp[i] = dp[i - 1] * target_arr[i]
+        return dp
+    
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
         n = len(nums)
-        ans = [] * n # ans[i]: i-1までの累積の積
-        dp = [1] * n
-        dp[0] = nums[0]
 
+        dp_prefix = [1] * n
+        dp_prefix[0] = nums[0]
         for i in range(1, n):
-            ans[i] *= nums[i - 1] * ans[i - 1]
-
-        suffix = 1
+            dp_prefix[i] = dp_prefix[i - 1] * nums[i]
+    
+        dp_suffix = [1] * n
+        dp_suffix[-1] = nums[-1]
         for i in range(n - 2, -1, -1):
-            suffix *= nums[i + 1]
-            ans[i] *= suffix
+            dp_suffix[i] = dp_suffix[i + 1] * nums[i]
+
+        ans = [0] * n
+        ans[0] = dp_suffix[1]
+        ans[-1] = dp_prefix[-2]
+        for i in range(1, n - 1):
+            ans[i] = dp_prefix[i - 1] * dp_suffix[i + 1]
 
         return ans
     
 s = Solution()
 print(s.productExceptSelf([1,2,3,4]))
 print(s.productExceptSelf([-1,1,0,-3,3]))
+print(s.productExceptSelf([2,4]))
