304091f885564fc0b31a7620ce88f375383b56dc
tttol <tkhstol.929@gmail.com>
2025-06-04 20:22:35 +0900

    fix

--- python/valid_parentheses.py
+++ python/valid_parentheses.py
@@ -1,18 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        p1 = {"(": 0, ")": 0}
-        p2 = {"{": 0, "}": 0}
-        p3 = {"[": 0, "]": 0}
+      stack = []
+      for _s in s:
+          if _s == "(" or _s == "{" or _s == "[":
+              stack.append(_s)
+          elif _s == ")" or _s == "}" or _s == "]":
+              if len(stack) == 0:
+                  return False
+              
+              open_bracket = stack.pop()
+              parenthese = open_bracket + _s
+              if parenthese != "()" and parenthese != "{}" and parenthese != "[]":
+                  return False
+      return len (stack) == 0
 
-        for _s in s:
-            if _s == "(" or _s == ")":
-                p1[_s] = p1.get(_s) + 1
-            elif _s == "{" or _s == "}":
-                p2[_s] = p2.get(_s) + 1
-            elif _s == "[" or _s == "]":
-                p3[_s] = p3.get(_s) + 1
-        return p1.get("(") == p1.get(")") and p2.get("{") == p2.get("}") and p3.get("[") == p3.get("]")
-  
 if __name__ == "__main__":
     s = Solution()
     print(s.isValid("()"))
